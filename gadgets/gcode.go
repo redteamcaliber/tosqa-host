f225a4434c915dd8e4d447acf2ff6aeea33b4be6
Jean-Claude Wippler <jc@wippler.nl>
2014-03-27 00:01:29 +0100

    add basic parser and interpreter

--- gadgets/gcode.go
+++ gadgets/gcode.go
@@ -3,6 +3,7 @@ package gcode
 
 import (
 	"fmt"
+	"sort"
 	"strconv"
 	"strings"
 
@@ -11,28 +12,27 @@ import (
 
 func init() {
 	flow.Registry["GcodeScanner"] = func() flow.Circuitry { return &GcodeScanner{} }
+	flow.Registry["GcodeParser"] = func() flow.Circuitry { return &GcodeParser{} }
+	flow.Registry["GcodeInterp"] = func() flow.Circuitry { return &GcodeInterp{} }
 }
 
-// GcodeScanner process G-code text lines and parses them into commands.
+// GcodeScanner processes G-code text lines and turns each line into a word map.
 type GcodeScanner struct {
 	flow.Gadget
 	In  flow.Input
 	Out flow.Output
 }
 
-type gcodeWord struct {
-}
-
 // Start scanning lines and convert each one to a map of G-code "words".
 func (g *GcodeScanner) Run() {
 	for m := range g.In {
 		if s, ok := m.(string); ok {
-			words, comment := g.parse(s)
-			if len(words) > 0 {
-				g.Out.Send(words)
-			}
+			words, comment := g.scan(s)
 			if len(comment) > 0 {
 				g.Out.Send(flow.Tag{"<comment>", comment})
+			}
+			if len(words) > 0 {
+				g.Out.Send(words)
 			}
 		} else {
 			g.Out.Send(m)
@@ -40,8 +40,8 @@ func (g *GcodeScanner) Run() {
 	}
 }
 
-func (g *GcodeScanner) parse(s string) (words map[string]float64, comment string) {
-	words = make(map[string]float64)
+func (g *GcodeScanner) scan(s string) (words map[string]float64, comment string) {
+	words = map[string]float64{}
 	i, n := 0, len(s)
 	u := strings.ToUpper(s)
 
@@ -56,11 +56,7 @@ func (g *GcodeScanner) parse(s string) (words map[string]float64, comment string
 		for i < n && strings.IndexByte("0123456789-.", s[i]) >= 0 {
 			i++
 		}
-		// remove leading 0's, so that "G01" will end up as "G1"
-		t := strings.TrimLeft(s[pos:i], "0")
-		if len(t) == 0 {
-			t = "0"
-		}
+		t := s[pos:i]
 		// return as string and as float
 		f, _ := strconv.ParseFloat(t, 64)
 		return t, f
@@ -76,6 +72,11 @@ func (g *GcodeScanner) parse(s string) (words map[string]float64, comment string
 			return
 		case 'G', 'M':
 			t, f := number()
+			// remove leading 0's, so that "G01" will end up as "G1"
+			t = strings.TrimLeft(t, "0")
+			if t == "" {
+				t = "0"
+			}
 			words[fmt.Sprintf("%c%s", cmd, t)] = f
 		default:
 			if 'A' <= cmd && cmd <= 'Z' {
@@ -85,4 +86,248 @@ func (g *GcodeScanner) parse(s string) (words map[string]float64, comment string
 		}
 	}
 	return
+}
+
+// see http://www.linuxcnc.org/docs/devel/html/gcode/overview.html
+// in particular, section 25: G Code Order of Execution
+
+// used to sort G and M commands in the order they need to be executed
+var wordOrder = initWordOrder()
+
+func initWordOrder() map[string]int {
+	wo := map[string]int{}
+	seq := 0
+
+	add := func(s string) {
+		for _, v := range strings.Split(s, " ") {
+			wo[v] = seq
+		}
+		seq++
+	}
+
+	//  O-word commands (optionally followed by a comment but no other words allowed on the same line)
+	add("O")
+	//  Comment (including message)
+	//  Set feed rate mode (G93, G94).
+	add("G93 G94")
+	//  Set feed rate (F).
+	add("F")
+	//  Set spindle speed (S).
+	add("S")
+	//  Select tool (T).
+	add("T")
+	//  HAL pin I/O (M62-M68).
+	add("M62 M63 M64 M65 M66 M67 M68")
+	//  Change tool (M6) and Set Tool Number (M61).
+	add("M6 M61")
+	//  Spindle on or off (M3, M4, M5).
+	add("M3 M4 M5")
+	//  Save State (M70, M73), Restore State (M72), Invalidate State (M71).
+	add("M70 M71 M72 M73")
+	//  Coolant on or off (M7, M8, M9).
+	add("M7 M8 M9")
+	//  Enable or disable overrides (M48, M49,M50,M51,M52,M53).
+	add("M48 M49 M50 M51 M52 M53")
+	//  User-defined Commands (M100-M199).
+	add("M100 M101 M102 M103 M104 M105 M106 M107 M108 M109")
+	add("M110 M111 M112 M113 M114 M115 M116 M117 M118 M119")
+	add("M120 M121 M122 M123 M124 M125 M126 M127 M128 M129")
+	add("M130 M131 M132 M133 M134 M135 M136 M137 M138 M139")
+	add("M140 M141 M142 M143 M144 M145 M146 M147 M148 M149")
+	add("M150 M151 M152 M153 M154 M155 M156 M157 M158 M159")
+	add("M160 M161 M162 M163 M164 M165 M166 M167 M168 M169")
+	add("M160 M161 M162 M163 M164 M165 M166 M167 M168 M169")
+	add("M170 M171 M172 M173 M174 M175 M176 M177 M178 M179")
+	add("M180 M181 M182 M183 M184 M185 M186 M187 M188 M189")
+	add("M190 M191 M192 M193 M194 M195 M196 M197 M198 M199")
+	//  Dwell (G4).
+	add("G4")
+	//  Set active plane (G17, G18, G19).
+	add("G17 G18 G19")
+	//  Set length units (G20, G21).
+	add("G20 G21")
+	//  Cutter radius compensation on or off (G40, G41, G42)
+	add("G40 G41 G42")
+	//  Cutter length compensation on or off (G43, G49)
+	add("G43 G49")
+	//  Coordinate system selection (G54, G55, G56, G57, G58, G59, G59.1, G59.2, G59.3).
+	add("G54 G55 G56 G57 G58 G59 G59.1 G59.2 G59.3")
+	//  Set path control mode (G61, G61.1, G64)
+	add("G61 G61.1 G64")
+	//  Set distance mode (G90, G91).
+	add("G90 G91")
+	//  Set retract mode (G98, G99).
+	add("G98 G99")
+	//  Go to reference location (G28, G30) or change coordinate system data (G10) or set axis offsets (G92, G92.1, G92.2, G94).
+	add("G10 G28 G30 G92 G92.1 G92.2 G94")
+	//  Perform motion (G0 to G3, G33, G38.x, G73, G76, G80 to G89), as modified (possibly) by G53.
+	add("G0 G1 G2 G3 G33 G38.2 G38.3 G38.4 G38.5 G73 G76")
+	add("G80 G81 G82 G83 G84 G85 G86 G87 G88 G89")
+	//  Stop (M0, M1, M2, M30, M60).
+	add("M0 M1 M2 M30 M60")
+	return wo
+}
+
+// GcodeParser parses word maps and produces commands.
+type GcodeParser struct {
+	flow.Gadget
+	In  flow.Input
+	Out flow.Output
+}
+
+// Start parsing word maps and produce the corresponding commands.
+func (g *GcodeParser) Run() {
+	for m := range g.In {
+		if words, ok := m.(map[string]float64); ok {
+			g.parse(words)
+		} else {
+			g.Out.Send(m)
+		}
+	}
+}
+
+// see section 15: Modal Groups
+const modalLimit = 16
+
+var modalGroups = []string{
+	"G4 G10 G28 G30 G53 G92 G92.1 G92.2 G92.3 ",
+	"G0 G1 G2 G3 G33 G38.2 G38.3 G38.4 G38.5 G73 G76 G80 G81 G82 G83 G84 G85 G86 G87 G88 G89",
+	"G17 G18 G19 G17.1 G18.1 G19.1",
+	"G90 G91",
+	"G90.1 G91.1 M0 M1 M2 M30 M60",
+	"G93 G94 G95 M6",
+	"G20 G21",
+	"G40 G41 G42 G41.1 G42.1 M3 M4 M5",
+	"G43 G43.1 G49 M7 M8 M9",
+	"M48 M49",
+	"G98 G99",
+	"",
+	"G54 G55 G56 G57 G58 G59 G59.1 G59.2 G59.3",
+	"G61 G61.1 G64",
+	"G96 G97",
+	"G7 G8",
+}
+
+// used to determine which modal group a G or M command belongs to
+var modalMap = initModalMap()
+
+func initModalMap() map[string]int {
+	if modalLimit != len(modalGroups) {
+		panic(fmt.Errorf("modalLimit should be %d !", len(modalGroups)))
+	}
+	mm := map[string]int{}
+	for k, v := range modalGroups {
+		for _, x := range strings.Split(v, " ") {
+			mm[x] = k
+		}
+	}
+	return mm
+}
+
+func (g *GcodeParser) parse(words map[string]float64) {
+	modals := [modalLimit]string{} // allows at most one in each modal group
+	actions := []string{}          // used to collect all "actionable" words
+
+	// pick out all the modal and non-modal words which need to be sorted
+	for k := range words {
+		if n, ok := modalMap[k]; ok {
+			modals[n] = k
+			delete(words, k)
+		} else if _, ok := wordOrder[k]; ok {
+			actions = append(actions, k)
+		}
+	}
+	// append the modal commands we found as also being actionable
+	for _, k := range modals {
+		if k != "" {
+			actions = append(actions, k)
+		}
+	}
+	// sort by the order indicated in the wordOrder map
+	sort.Sort(byOrder(actions))
+
+	g.Out.Send(words)
+	g.Out.Send(actions)
+}
+
+type byOrder []string
+
+func (a byOrder) Len() int           { return len(a) }
+func (a byOrder) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a byOrder) Less(i, j int) bool { return wordOrder[a[i]] < wordOrder[a[j]] }
+
+// GcodeInterp interprets word maps and action lists to generate commands.
+type GcodeInterp struct {
+	flow.Gadget
+	In  flow.Input
+	Out flow.Output
+
+	last [4]float64
+}
+
+const (
+	cX = iota
+	cY
+	cZ
+	cF
+)
+
+// Start interpreting the word maps and action lists from the parser.
+func (g *GcodeInterp) Run() {
+	words := map[string]float64{}
+
+	for m := range g.In {
+		switch v := m.(type) {
+		case (map[string]float64):
+			words = v
+		case ([]string):
+			g.process(words, v)
+		default:
+			g.Out.Send(m)
+		}
+	}
+}
+
+func (g *GcodeInterp) process(words map[string]float64, actions []string) {
+	next := g.last
+	cmd := ""
+
+	setCoord := func(x string, i int) {
+		if v, ok := words[x]; ok {
+			next[i] = v
+		}
+	}
+
+	setCoord("X", cX)
+	setCoord("Y", cY)
+	setCoord("Z", cZ)
+
+	emitNext := func() {
+		g.Out.Send(flow.Tag{cmd, next})
+		g.last = next
+	}
+
+	emitValue := func(value interface{}) {
+		g.Out.Send(flow.Tag{cmd, value})
+	}
+
+	for _, cmd = range actions {
+		value := words[cmd]
+		switch cmd {
+		case "F":
+			next[cF] = value
+		case "S":
+			emitValue(value)
+		case "G0":
+			rapid := next
+			rapid[cF] = 1e9
+			emitValue(rapid)
+		case "G1":
+			emitNext()
+		case "M30":
+			emitValue(nil)
+		default:
+			g.Out.Send(flow.Tag{"<ignored>", cmd})
+		}
+	}
 }
--- gadgets/gcode_test.go
+++ gadgets/gcode_test.go
@@ -6,10 +6,69 @@ import (
 
 func ExampleGcodeScanner() {
 	c := flow.NewCircuit()
-	c.Add("gp", "GcodeScanner")
-	c.Feed("gp.In", "G01 X2.3 Y04.5 Z-6.7 ;gcode example")
+	c.Add("s", "GcodeScanner")
+	c.Feed("s.In", "G01 X2.3 Y04.5 Z-6.7 ;gcode example")
 	c.Run()
 	// Output:
-	// Lost map[string]float64: map[G1:1 X:2.3 Y:4.5 Z:-6.7]
 	// Lost flow.Tag: {<comment> gcode example}
+	// Lost map[string]float64: map[G1:1 X:2.3 Y:4.5 Z:-6.7]
+}
+
+func ExampleGcodeParser() {
+	c := flow.NewCircuit()
+	c.Add("s", "GcodeScanner")
+	c.Add("p", "GcodeParser")
+	c.Connect("s.Out", "p.In", 0)
+	c.Feed("s.In", "G01 X2.3 Y04.5 Z-6.7 ;gcode example")
+	c.Run()
+	// Output:
+	// Lost flow.Tag: {<comment> gcode example}
+	// Lost map[string]float64: map[X:2.3 Y:4.5 Z:-6.7]
+	// Lost []string: [G1]
+}
+
+func ExampleGcodeInterp() {
+	c := flow.NewCircuit()
+	c.Add("s", "GcodeScanner")
+	c.Add("p", "GcodeParser")
+	c.Add("i", "GcodeInterp")
+	c.Connect("s.Out", "p.In", 0)
+	c.Connect("p.Out", "i.In", 0)
+	c.Feed("s.In", "G00 X6.139 Y11.245 Z8.100")
+	c.Feed("s.In", "G01 Z-2.000 F600 S10000  ")
+	c.Feed("s.In", "G01 Y78.716 F1000        ")
+	c.Feed("s.In", "G01 X6.261 Y79.810       ")
+	c.Feed("s.In", "G01 X6.625 Y80.904       ")
+	c.Feed("s.In", "G01 X86.765 Y35.118      ")
+	c.Feed("s.In", "G01 X87.118 Y35.588      ")
+	c.Feed("s.In", "G00 Z8.000               ")
+	c.Feed("s.In", "G00 X14.059 Y64.059      ")
+	c.Feed("s.In", "G01 Z-26.000 F600        ")
+	c.Feed("s.In", "G01 X85.941 F1000        ")
+	c.Feed("s.In", "G00 Z8.000               ")
+	c.Feed("s.In", "G00 X14.059 Y25.941      ")
+	c.Feed("s.In", "G01 Z-26.000 F600        ")
+	c.Feed("s.In", "G01 X86.754 F1000        ")
+	c.Feed("s.In", "G00 Z8.000               ")
+	c.Feed("s.In", "M30                      ")
+	c.Run()
+	// Output:
+	// Lost flow.Tag: {G0 [6.139 11.245 8.1 1e+09]}
+	// Lost flow.Tag: {S 10000}
+	// Lost flow.Tag: {G1 [0 0 -2 600]}
+	// Lost flow.Tag: {G1 [0 78.716 -2 1000]}
+	// Lost flow.Tag: {G1 [6.261 79.81 -2 1000]}
+	// Lost flow.Tag: {G1 [6.625 80.904 -2 1000]}
+	// Lost flow.Tag: {G1 [86.765 35.118 -2 1000]}
+	// Lost flow.Tag: {G1 [87.118 35.588 -2 1000]}
+	// Lost flow.Tag: {G0 [87.118 35.588 8 1e+09]}
+	// Lost flow.Tag: {G0 [14.059 64.059 -2 1e+09]}
+	// Lost flow.Tag: {G1 [87.118 35.588 -26 600]}
+	// Lost flow.Tag: {G1 [85.941 35.588 -26 1000]}
+	// Lost flow.Tag: {G0 [85.941 35.588 8 1e+09]}
+	// Lost flow.Tag: {G0 [14.059 25.941 -26 1e+09]}
+	// Lost flow.Tag: {G1 [85.941 35.588 -26 600]}
+	// Lost flow.Tag: {G1 [86.754 35.588 -26 1000]}
+	// Lost flow.Tag: {G0 [86.754 35.588 8 1e+09]}
+	// Lost flow.Tag: {M30 <nil>}
 }
